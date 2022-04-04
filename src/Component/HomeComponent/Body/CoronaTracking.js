520a430d3d6c149c486b41f6832d422b8aadcd35
NTTan98 <nguyenthanhtan057@gmail.com>
2022-04-05 01:22:42 +0700

    corona tracking

--- src/Component/HomeComponent/Body/CoronaTracking.js
+++ src/Component/HomeComponent/Body/CoronaTracking.js
@@ -1,28 +1,89 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { url } from '../../../api/CoronaAPI.js';
-import { Grid, Input } from '@chakra-ui/react';
+import { Grid, Heading, Input } from '@chakra-ui/react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import _ from 'lodash';
+ChartJS.register(ArcElement, Tooltip, Legend);
 const CoronaTracking = () => {
-  const [corona, setCorona] = useState([]);
+  const [corona, setCorona] = useState(null);
   const [search, setSearch] = useState('');
   const [searchResult, setSearchResult] = useState([]);
   useEffect(() => {
-    axios
-      .get(url)
-      .then(res => {
-        setCorona(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-      set
+    const fetchData = async () => {
+      const result = await axios(url);
+      setCorona(result.data);
+    };
+    fetchData();
   }, []);
 
-  console.log(corona);
+  const options = {
+    maintainAspectRatio: false,
+    legend: {
+      display: false,
+    },
+    tooltips: {
+      enabled: false,
+    },
+  };
+  let data;
+  if (corona !== null) {
+    if (searchResult.length > 0) {
+      data = {
+        labels: ['Cases', 'Deaths', 'Recovered'],
+        datasets: [
+          {
+            label: 'Corona',
+            backgroundColor: ['#e11f11', '#36A2EB', '#FFCE56'],
+            hoverBackgroundColor: ['#e11f11', '#36A2EB', '#FFCE56'],
+            data: [
+              searchResult[0].cases,
+              searchResult[0].deaths,
+              searchResult[0].recovered,
+            ],
+          },
+        ],
+        name: searchResult[0].country,
+      };
+    } else {
+      data = {
+        labels: ['Cases', 'Deaths', 'Recovered'],
+        datasets: [
+          {
+            label: 'Corona',
+            backgroundColor: ['#e11f11', '#36A2EB', '#FFCE56'],
+            hoverBackgroundColor: ['#e11f11', '#36A2EB', '#FFCE56'],
+            data: [corona[0].cases, corona[0].deaths, corona[0].recovered],
+          },
+        ],
+        name: corona[0].country,
+      };
+    }
+  }
   return (
-    <div>
-      <h1>CoronaTracking</h1>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '2rem',
+      }}
+    >
+      <Heading
+        as="h3"
+        size="lg"
+        style={{
+          marginBottom: '1rem',
+          color: '#e11f11',
+          fontWeight: 'bold',
+        }}
+      >
+        Where do you want tracking Covid 19
+      </Heading>
       <Input
+        value={search}
         placeholder="Search"
         onChange={e => {
           setSearch(e.target.value);
@@ -36,17 +97,35 @@ const CoronaTracking = () => {
                   .includes(search.toLowerCase());
               })
             );
+            setSearch('');
           }
         }}
+        style={{
+          width: '50%',
+          marginBottom: '1rem',
+        }}
       />
-      <Grid
-        templateColumns="repeat(auto-fit, minmax(200px, 1fr))"
-        gap={4}
-        mt={4}
-        p={4}
+      <Heading
+        as="h5"
+        size="md"
+        style={{
+          color: '#e11f11',
+          fontWeight: 'bold',
+        }}
       >
-        {/* <p1>{searchResult[0].country}</p1> */}
-      </Grid>
+        COVID 19 Chart{' Of '}
+        {corona !== null ? data.name : ''}
+      </Heading>
+      <div
+        style={{
+          marginTop: '1rem',
+          width: '100%',
+          height: '400px',
+          marginBottom: '1rem',
+        }}
+      >
+        {corona !== null ? <Doughnut data={data} options={options} /> : ''}
+      </div>
     </div>
   );
 };
